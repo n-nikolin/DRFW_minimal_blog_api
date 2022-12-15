cef74f60ddc3edaa21753a7caed541ed92f052df
Nikita N <nikitanikolin85@gmail.com>
2022-12-15 03:04:10 +0300

    added cdeditor to admin site

--- api/api/settings.py
+++ api/api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,8 +40,17 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'blog'
+    'ckeditor',
+    'ckeditor_uploader',
+    'blog',
 ]
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+#ckeditor upload path
+CKEDITOR_UPLOAD_PATH="uploads/"
+# CKEDITOR_BASEPATH = "/my_static/ckeditor/ckeditor/"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- api/blog/models.py
+++ api/blog/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField 
 
 class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     last_edit = models.DateTimeField(auto_now=True)
     title= models.CharField(max_length=100, blank=False)
-    content=models.TextField()
+    content=RichTextUploadingField()
 
     class Meta:
         ordering=['created']
