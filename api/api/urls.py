a9eb787a235178bb60ebe4f524f4a5d273684221
Nikita N <nikitanikolin85@gmail.com>
2022-12-17 21:05:14 +0300

    fixed urls

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,8 +1,6 @@
-from django.urls import include, path, re_path
+from django.urls import include, path
 from django.contrib import admin
 from rest_framework import routers
-from django.conf.urls.static import static
-from django.conf import settings
 from blog import views
 
 router = routers.DefaultRouter()
@@ -11,7 +9,4 @@ router.register(r'posts', views.PostViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    re_path(r'^ckeditor/', include('ckeditor_uploader.urls')), # The CKEditor path
 ]
-
-urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
